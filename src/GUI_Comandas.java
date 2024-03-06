660d7b2631dd92799b6744fa2ed8464b5fec48c3
Joseeduardoacgarcia <Joseeduardoacgarcia@gmail.com>
2024-03-05 21:41:35 -0600

    2da Version de la GUI

--- src/GUI_Comandas.java
+++ src/GUI_Comandas.java
@@ -13,10 +13,6 @@ public class GUI_Comandas extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -47,7 +43,7 @@ public class GUI_Comandas extends JFrame {
 		contentPane.setLayout(null);
 		
 		JList list = new JList();
-		list.setBounds(724, 104, 232, 410);
+		list.setBounds(424, 87, 452, 410);
 		contentPane.add(list);
 		
 		JLabel lblNewLabel = new JLabel("Sistema de Comandas");
@@ -55,59 +51,19 @@ public class GUI_Comandas extends JFrame {
 		lblNewLabel.setBounds(272, 23, 393, 53);
 		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(356, 136, 289, 36);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(356, 187, 289, 36);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(356, 246, 289, 36);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(356, 309, 289, 36);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("Orden");
-		lblNewLabel_1.setFont(new Font("Impact", Font.ITALIC, 25));
-		lblNewLabel_1.setBounds(22, 136, 208, 36);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("Cantidad");
-		lblNewLabel_2.setFont(new Font("Impact", Font.ITALIC, 25));
-		lblNewLabel_2.setBounds(22, 187, 269, 36);
-		contentPane.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("Precio");
-		lblNewLabel_3.setFont(new Font("Impact", Font.ITALIC, 25));
-		lblNewLabel_3.setBounds(23, 246, 310, 36);
-		contentPane.add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_4 = new JLabel("Estado");
-		lblNewLabel_4.setFont(new Font("Impact", Font.ITALIC, 25));
-		lblNewLabel_4.setBounds(23, 309, 227, 36);
-		contentPane.add(lblNewLabel_4);
-		
-		JButton btnNewButton = new JButton("Agregar Comanda");
+		JButton btnNewButton = new JButton("Mostrar Comandas");
 		btnNewButton.setFont(new Font("Impact", Font.ITALIC, 15));
-		btnNewButton.setBounds(56, 410, 163, 53);
+		btnNewButton.setBounds(150, 161, 163, 53);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Enviar a Cocina");
 		btnNewButton_1.setFont(new Font("Impact", Font.ITALIC, 15));
-		btnNewButton_1.setBounds(265, 410, 163, 53);
+		btnNewButton_1.setBounds(150, 270, 163, 53);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Status de Comandas");
 		btnNewButton_2.setFont(new Font("Impact", Font.ITALIC, 15));
-		btnNewButton_2.setBounds(469, 410, 163, 53);
+		btnNewButton_2.setBounds(150, 377, 163, 53);
 		contentPane.add(btnNewButton_2);
 	}
 
